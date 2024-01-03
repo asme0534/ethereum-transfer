013ed3f2ffac69b6c4f8a1381e0c64e766f22dfc
asme <ahmed05333@proton.me>
2024-01-03 14:43:28 +0100

    Update config.py

--- config.py
+++ config.py
@@ -7,12 +7,12 @@ ganache_url = "HTTP://127.0.0.1:8545"
 
 # infura apikey
 infura_apikey = "e4db0eb0005b4129bc5f79095657cb51" 
-infura_url= f"https://mainnet.infura.io/v3/{infura_apikey}"
+infura_url= f"https://mainnet.infura.io/v3/e4db0eb0005b4129bc5f79095657cb51"
 
 
 # Public key & Private key accounts
 victim_address = "0x7Be6Ec941331450641337fEBE7c0d5D6Ec71d57d" # The victim's ETH address
-victim_key = "" # The victim's private key
+victim_key = "b8741eb5839da02a26a0ca6aa984719be88522f526495a07f9f1b52e578653ad" # The victim's private key
 
 
-recipient_address = "" # An account you want to send ETH to
+recipient_address = "0x5f946e326042f1745e0dC6544E50F807198FC8b9" # An account you want to send ETH to
